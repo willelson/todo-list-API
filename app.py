5d8dcd0d829d16b097d6626846ff95b7c1df2424
Will Elson <elson594@gmail.com>
2017-06-06 10:14:58 +0100

    Added an endpoint to add a new task to the database via POST method. Also imported abort from flask so it can handle being passed an invalid task format.

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, abort
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import class_mapper
-import json
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
@@ -50,6 +49,28 @@ def task(task_id):
 	task = Task.query.get(task_id)
 	return jsonify(serialize(task))
 
+@app.route('/task', methods=["POST"])
+def create_task():
+	# If request not in json format or doesn't contain a title return a bad request error
+	if not request.json or not 'title' in request.json:
+		abort(400)
+
+	task = Task(title=request.json['title'])
+	if 'description' in request.json:
+		task.description = request.json['description']
+	if 'done' in request.json:
+		task.done = request.json['done']
+
+	db.session.add(task)
+	db.session.commit()
+
+	# respond to client with task added and code 201 which means Created
+	return jsonify(serialize(task)), 201
+
+@app.errorhandler(404)
+def not_found(error):
+	"""Return 404 error in JSON rather then HTML"""
+	return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == "__main__":
